2842c74483a4d5270b4f193ffe6a67996ed18f15
Ryan Golden <ryan.john.golden@gmail.com>
2012-05-24 13:33:34 -0500

    Updated order of ubuntu script

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-curl https://raw.github.com/rjg/server-scripts/master/install-ruby.sh | bash
 curl https://raw.github.com/rjg/server-scripts/master/install-dotfiles.sh | bash
+curl https://raw.github.com/rjg/server-scripts/master/install-ruby.sh | bash
 curl https://raw.github.com/rjg/server-scripts/master/install-vim.sh | bash
