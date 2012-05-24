500d78cbe941eb640c918367a53aa98402832275
Ryan Golden <ryan.john.golden@gmail.com>
2012-05-24 11:46:15 -0500

    Updated dotfile install script

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 # Load ruby
-if [[ -d $HOME/.rbenv ]]; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-fi
+sudo apt-get -y install ruby1.9.1
+
+# Installing Rake
+sudo gem install rake # Dep for dotfile install script 
+sudo gem install bundler # Dep for command-t install
 
 # install hub
 echo "INSTALLING HUB"
@@ -13,27 +14,27 @@ curl http://defunkt.io/hub/standalone -sLo ~/bin/hub && chmod +x ~/bin/hub
 
 # Setting up dotfiles
 echo "INSTALLING DOTFILES"
-mkdir ~/code
-cd ~/code
+mkdir ~/code && cd ~/code
 git clone https://github.com/rjg/dotfiles
 cd ~/code/dotfiles
 git checkout -t remote/linode
 rake install
-exec $SHELL
 
-# nokogiri requirements
+# Nokogiri requirements
 sudo apt-get -y install libxslt-dev libxml2-dev
 
 # Getting vim plugins
 cd ~/code/dotfiles/vim/ && rm -rf bundle && mkdir bundle && cd bundle
-git clone tpope/vim-surround 
-git clone tpope/vim-rake
-git clone tpope/vim-ragtag
-git clone godlygeek/csapprox 
-git clone tpope/vim-commentary
-git clone tpope/vim-fugitive
-git clone tpope/vim-rails
-git clone wincent/Command-T command-t
-cd command-t
+git clone https://github.com/tpope/vim-surround 
+git clone https://github.com/tpope/vim-rake
+git clone https://github.com/tpope/vim-ragtag
+git clone https://github.com/godlygeek/csapprox 
+git clone https://github.com/tpope/vim-commentary
+git clone https://github.com/tpope/vim-fugitive
+git clone https://github.com/tpope/vim-rails
+git clone https://github.com/wincent/Command-T command-t
+cd ~/code/dotfiles/vim/bundle/command-t
 bundle install --path vendor/bundle
 rake make
+
+exec $SHELL
